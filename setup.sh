abb0a344076f04392c9fa3ba369f8edd32bc7957
Romualdo D. Atibula <riidatibula@gmail.com>
2021-11-10 14:47:33 +0800

    created setup file

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Must have a working cardano-cli build
+# preferred path: $HOME/.local/bin
+
+# Create main directory
+echo "Creating main directory for LuckyFive utilities"
+main_dir=$HOME/.luckyfive
+mkdir $main_dir
+
+# directory for policy scripts
+echo "Creating directory for policy scripts"
+mkdir $main_dir/policy
+
+# diretory for payment keys
+echo "Creating directory for payment keys"
+mkdir $main_dir/payment
+
+# create payment key pair
+# TODO
+
+echo "Setup done!"
