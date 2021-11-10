a783a6750f589f53faccf8870117b8f8d3023de2
Romualdo D. Atibula <riidatibula@gmail.com>
2021-11-10 15:31:52 +0800

    created cardano module

--- src/bets/cron/lottery.py
+++ src/bets/cron/lottery.py
@@ -2,19 +2,18 @@ import subprocess
 
 from django.conf import settings
 from bets.models import Lottery
+from bets.cardano.operations import (
+    generate_minting_policy)
 
 
 # Every Monday 00:00 UTC
 def new_lottery():
     # Generate policy script for minting
+    policyID = generate_minting_policy()
+    print(policyID)
 
     # Create a lottery object every week
-    process = subprocess.run(
-        ['which', 'cardano-cli'],
-        capture_output=True,
-        text=True)
-    print(process)
-    print(process.stdout)
+    #Lottery.objects.create()
     pass
 
 
