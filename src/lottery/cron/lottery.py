7aa3492447ac678a6dcc9130033c656eff46388d
Romualdo D. Atibula <riidatibula@gmail.com>
2021-12-13 15:10:56 +0800

    added error checking for lottery crons

--- src/lottery/cron/lottery.py
+++ src/lottery/cron/lottery.py
@@ -12,38 +12,45 @@ from lottery.cardano.operations import (
 
 # Every Monday 00:00 UTC
 def new_lottery():
-    # Create directory for the new policy
-    today = datetime.now(timezone.utc)
-    policy_path = settings.POLICY_DIR + \
-        today.strftime('%m_%d_%y')
-    subprocess.run(['mkdir', '-p', policy_path])
+    try:
+        if Lottery.get_current_lottery():
+            raise Exception(
+                'There\'s still an ongoing lottery')
 
-    # Generate minting policy for the
-    # ticket nfts of this lottery
-    policyID = generate_minting_policy(policy_path)
+        # Create directory for the new policy
+        today = datetime.now(timezone.utc)
+        policy_path = settings.POLICY_DIR + \
+            today.strftime('%m_%d_%y')
+        subprocess.run(['mkdir', '-p', policy_path])
 
-    # Get lottery draw date
-    offset = 6 - today.weekday()
-    lottery_day = today + timedelta(days=offset)
-    draw_date = datetime(
-        lottery_day.year, lottery_day.month,
-        lottery_day.day,
-        hour=9, minute=0,
-        second=0, microsecond=0,
-        tzinfo=timezone.utc
-    )
+        # Generate minting policy for the
+        # ticket nfts of this lottery
+        policyID = generate_minting_policy(policy_path)
 
-    # Get lotter sequence
-    seq = 1
-    lastest_lottery = Lottery.objects.last()
-    if lastest_lottery:
-        seq = lastest_lottery.seq + 1
+        # Get lottery draw date
+        offset = 6 - today.weekday()
+        lottery_day = today + timedelta(days=offset)
+        draw_date = datetime(
+            lottery_day.year, lottery_day.month,
+            lottery_day.day,
+            hour=9, minute=0,
+            second=0, microsecond=0,
+            tzinfo=timezone.utc
+        )
 
-    # Create lottery object
-    lottery = Lottery.objects.create(
-        policy_id=policyID,
-        draw_date=draw_date,
-        seq=seq)
+        # Get lotter sequence
+        seq = 1
+        latest_lottery = Lottery.objects.last()
+        if latest_lottery:
+            seq = latest_lottery.seq + 1
+
+        # Create lottery object
+        lottery = Lottery.objects.create(
+            policy_id=policyID,
+            draw_date=draw_date,
+            seq=seq)
+    except Exception as e:
+        print(e)
 
 
 # Every Sunday 09:00 UTC
@@ -66,47 +73,48 @@ def draw_winners():
         'id': 1
     }
 
-    while (status_code != 200):
+    try:
+        # Get current lottery
+        current_lottery = Lottery.get_current_lottery()
+        if current_lottery is None:
+            raise Exception('No active lottery')
+        print('Drawing Luckyfive for: {}'.format(
+            current_lottery))
+
         response = requests.post(
             'https://api.random.org/json-rpc/4/invoke',
             data=json.dumps(payload),
             headers=headers)
         status_code = response.status_code
+        response_dict = response.json()
 
-        if status_code == 200:
-            response_dict = response.json()
+        if 'error' in response_dict.keys():
+            error_obj = response_dict.get('error')
+            status_code = error_obj.get('code')
+            message = error_obj.get('message')
+            raise Exception(message)
+        elif status_code == 200:
+            print('Request Successful!')
 
-            if 'error' in response_dict.keys():
-                status_code = 500
-                print('Error processing request. Retrying.')
-            else:
-                print('Request Successful!')
+            # Draw lottery winners
+            lucky_five = response_dict.get(
+                'result').get('random').get('data')[0]
+            print('Luckyfive is: {}'.format(lucky_five))
 
-                # Get current lottery
-                # TODO: add check if there's no current lottery
-                current_lottery = Lottery.get_current_lottery()
-                current_lottery.api_response = response_dict
-                print('Drawing Luckyfive for: {}'.format(
-                    current_lottery))
+            winning_bets = current_lottery.active_bets.filter(
+                lucky_five=lucky_five)
+            lottery_winners = list(LotteryWinner(
+                bet=bet, lottery=current_lottery)
+                for bet in winning_bets)
+            LotteryWinner.objects.bulk_create(
+                lottery_winners)
+            print('Lottery Winners: {}'.format(lottery_winners))
 
-                # Draw lottery winners
-                lucky_five = response_dict.get(
-                    'result').get('random').get('data')[0]
-                print('Luckyfive is: {}'.format(lucky_five))
-
-                winning_bets = current_lottery.active_bets.filter(
-                    lucky_five=lucky_five)
-                lottery_winners = list(LotteryWinner(
-                    bet=bet, lottery=current_lottery)
-                    for bet in winning_bets)
-                print('Lottery Winners: {}'.format(lottery_winners))
-                LotteryWinner.objects.bulk_create(
-                    lottery_winners)
-
-                # Deactivate current lottery
-                current_lottery.is_active = False
-                current_lottery.save()
+            # Deactivate current lottery
+            current_lottery.api_response = response_dict
+            current_lottery.is_active = False
+            current_lottery.save()
         else:
-            print('Error processing request. Retrying.')
-
-    pass
+            raise Exception('Unable to process request. Retrying.')
+    except Exception as e:
+        print(e)
