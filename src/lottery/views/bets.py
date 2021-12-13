c8d891fe218a1d3309644b3cb14e930b2ddf05ff
Rom Atibula <riidatibula@gmail.com>
2021-12-13 23:01:45 +0800

    fixed bets list bug

--- src/lottery/views/bets.py
+++ src/lottery/views/bets.py
@@ -10,6 +10,7 @@ class BetsListView(ListView):
     paginate_by = 10
     ordering = 'pk'
     queryset = Bet.objects.all()
+    template_name = 'lottery/bet_list.html'
 
     def get_queryset(self):
         queryset = self.queryset
@@ -33,30 +34,38 @@ class BetsListView(ListView):
         return queryset
 
     def get(self, request, *args, **kwargs):
-        self.object_list = self.get_queryset()
-        allow_empty = self.get_allow_empty()
-        lottery_seq = self.request.GET.get(
-            'lottery')
+        context = {}
+        try:
+            lottery_seq = self.request.GET.get(
+                'lottery')
+            lottery = Lottery.get_current_lottery()
+            if lottery_seq:
+                lottery = get_object_or_404(
+                    Lottery, seq=int(lottery_seq))
 
-        # TODO: add check if there's no current lottery
-        lottery = Lottery.get_current_lottery()
-        if lottery_seq:
-            lottery = get_object_or_404(
-                Lottery, seq=int(lottery_seq))
+            if lottery is None:
+                raise Exception('No active lottery')
 
-        if not allow_empty:
-            # When pagination is enabled and object_list is a queryset,
-            # it's better to do a cheap query than to load the unpaginated
-            # queryset in memory.
-            if self.get_paginate_by(self.object_list) is not None and hasattr(self.object_list, 'exists'):
-                is_empty = not self.object_list.exists()
-            else:
-                is_empty = not self.object_list
-            if is_empty:
-                raise Http404(_('Empty list and “%(class_name)s.allow_empty” is False.') % {
-                    'class_name': self.__class__.__name__,
-                })
-        context = self.get_context_data()
-        context['lottery'] = lottery
+            self.object_list = self.get_queryset()
+            allow_empty = self.get_allow_empty()
+
+            if not allow_empty:
+                # When pagination is enabled and object_list is a queryset,
+                # it's better to do a cheap query than to load the unpaginated
+                # queryset in memory.
+                if self.get_paginate_by(self.object_list) is not None and hasattr(self.object_list, 'exists'):
+                    is_empty = not self.object_list.exists()
+                else:
+                    is_empty = not self.object_list
+                if is_empty:
+                    raise Http404(_('Empty list and “%(class_name)s.allow_empty” is False.') % {
+                        'class_name': self.__class__.__name__,
+                    })
+            context = self.get_context_data()
+            context['lottery'] = lottery
+
+        except Exception as e:
+            context = {'error': e}
+            return render(request, self.template_name, context)
 
         return self.render_to_response(context)
