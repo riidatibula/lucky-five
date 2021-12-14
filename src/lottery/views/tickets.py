e3adf5270e9201d9d5e1f9aeb22048b891de2658
Romualdo D. Atibula <riidatibula@gmail.com>
2021-12-14 16:42:28 +0800

    renamed buyticket html file

--- src/lottery/views/tickets.py
+++ src/lottery/views/tickets.py
@@ -3,7 +3,7 @@ from django.views import View
 
 
 class BuyTicketView(View):
-    template_name = 'lottery/buyticket.html'
+    template_name = 'lottery/buy_ticket.html'
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {})
