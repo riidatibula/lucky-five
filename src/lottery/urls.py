39bd2d43cb21ea85d9024e6f16860b0c146996ed
Rom Atibula <riidatibula@gmail.com>
2021-12-22 20:35:10 +0800

    wip

--- src/lottery/urls.py
+++ src/lottery/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from lottery.views.home import HomeView
-from lottery.views.tickets import BuyTicketView
+from lottery.views.tickets import (
+    AcceptBuyTicketNotice, BuyTicketView)
 from lottery.views.bets import BetsListView
 from lottery.views.lotteries import (
     LotteryListView, LotteryDetailView)
@@ -11,6 +12,7 @@ app_name = 'lottery'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('buy-ticket/', BuyTicketView.as_view(), name='buy-ticket'),
+    path('accept-buy-ticket-notice/', AcceptBuyTicketNotice.as_view(), name='accept-notice'),
     path('bets/', BetsListView.as_view(), name='bet-list'),
     path('lotteries/', LotteryListView.as_view(), name='lottery-list'),
     path('lotteries/<int:pk>/', LotteryDetailView.as_view(), name='lottery-detail')
--- src/lottery/views/tickets.py
+++ src/lottery/views/tickets.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import JsonResponse
+
+
+class AcceptBuyTicketNotice(View):
+
+    def get(self, request, *args, **kwargs):
+        try:
+            if request.is_ajax():
+                wallet_check = self.request.GET.get('walletCheck')
+                terms_check = self.request.GET.get('termsCheck')
+
+                if wallet_check != 'true':
+                    raise Exception(
+                        'Please accept wallet condition.')
+                if terms_check != 'true':
+                    raise Exception(
+                        'Please accept terms and condition.')
+
+                return JsonResponse(data = {'data': 'yey', 'status': 200})
+            raise Exception('Unable to process request.')
+
+        except Exception as e:
+            return JsonResponse(data = {'error': str(e), 'status': 500})
 
 
 class BuyTicketView(View):
